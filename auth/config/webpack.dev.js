a0b1b3a6fa4239512f1312328a101f5a64d57438
Patricia Sawamura <patricia.sawamura@gmail.com>
2021-05-05 14:22:44 +0200

    added auth flow

--- auth/config/webpack.dev.js
+++ auth/config/webpack.dev.js
@@ -1,26 +1,26 @@
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const packageJson = require('../package.json');
 const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const devConfig = {
   mode: 'development',
   output: {
-    publicPath: 'http://localhost:8083/',
+    publicPath: 'http://localhost:8084/',
   },
   devServer: {
-    port: 8083,
+    port: 8084,
     historyApiFallback: {
       index: 'index.html',
     },
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'marketing',
+      name: 'auth',
       filename: 'remoteEntry.js',
       exposes: {
-        './MarketingApp': './src/bootstrap',
+        './AuthApp': './src/bootstrap',
       },
       shared: packageJson.dependencies,
     }),
@@ -28,6 +28,6 @@ const devConfig = {
       template: './public/index.html',
     }),
   ],
-}
+};
 
 module.exports = merge(commonConfig, devConfig);
--- auth/src/bootstrap.js
+++ auth/src/bootstrap.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
-export const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+// Mount function to start up the app
+const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
   const history = defaultHistory || createMemoryHistory({
     initialEntries: [initialPath],
   });
@@ -11,22 +12,30 @@ export const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
   if (onNavigate) {
     history.listen(onNavigate);
   }
-  ReactDOM.render(<App history={history}/>, el);
+
+  ReactDOM.render(<App onSignIn={onSignIn} history={history} />, el);
 
   return {
     onParentNavigate({ pathname: nextPathname }) {
       const { pathname } = history.location;
+
       if (pathname !== nextPathname) {
         history.push(nextPathname);
       }
-    }
-  }
+    },
+  };
 };
 
+// If we are in development and in isolation,
+// call mount immediately
 if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_marketing-dev-root');
+  const devRoot = document.querySelector('#_auth-dev-root');
 
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
+
+// We are running through container
+// and we should export the mount function
+export { mount };
